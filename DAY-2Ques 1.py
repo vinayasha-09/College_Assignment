c7fccab720b3425bf8db75f5a9a273c7032efe00
vinayasha-09 <vinayasha1982@gmail.com>
2026-05-31 16:19:10 +0530

    Add files via upload

---
+++ DAY-2Ques 1.py
@@ -0,0 +1,4 @@
+# write a program to find the sum of digit in a no.
+
+num = input("Enter a number: ")
+print("Sum of digits:", sum(int(digit) for digit in num))
---
+++ DAY-2Ques 2.py
@@ -0,0 +1,4 @@
+#write a program to reverse the no.
+
+num = input("Enter a number: ")
+print("Reversed number is:", num[::-1])
