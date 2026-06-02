830ab04b2a44e3d6a0a5f3e212fdad76d6a979be
vinayasha-09 <vinayasha1982@gmail.com>
2026-06-02 10:50:32 +0530

    Removing files

--- DAY-2/DAY-2Ques 1.py
+++
@@ -1,2 +0,0 @@
-num = input("Enter a number: ")
-print("Sum of digits =", sum(int(digit) for digit in num))
--- DAY-2/DAY-2Ques 3.py
+++
@@ -1,7 +0,0 @@
-# write a program to find the product of digit in a no.
-
-num = input("Enter a number: ")
-product = 1
-for digit in num:
-    product *= int(digit)
-print("Product of digits:", product)
