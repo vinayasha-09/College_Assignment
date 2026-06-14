2f6aebb7e07f626b6f584cbcb599bb5198b085bd
vinayasha-09 <vinayasha1982@gmail.com>
2026-06-14 12:36:24 +0530

    DAY-17 uploaded

---
+++ DAY-17/DAY-17Ques 1.py
@@ -0,0 +1,13 @@
+# Write a program to Merge arrays.
+
+# Input arrays
+arr1 = [1, 2, 3, 4]
+arr2 = [5, 6, 7, 8]
+
+# Merge arrays
+merged_array = arr1 + arr2
+
+# result
+print("First array:", arr1)
+print("Second array:", arr2)
+print("Merged array:", merged_array)
---
+++ DAY-17/DAY-17Ques 2.py
@@ -0,0 +1,13 @@
+# Write a program to Union of arrays.
+
+# Input arrays
+arr1 = [1, 2, 3, 4]
+arr2 = [3, 4, 5, 6]
+
+# Union using set
+union_array = list(set(arr1) | set(arr2))
+
+# result
+print("First array:", arr1)
+print("Second array:", arr2)
+print("Union of arrays:", union_array)
