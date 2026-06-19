45e05dc314764304ba0a9d74f4ec7e9df133b129
vinayasha-09 <vinayasha1982@gmail.com>
2026-06-19 11:41:05 +0530

    DAY-1 uploaded

---
+++ DAY-1/DAY-1Ques 1.py
@@ -0,0 +1,6 @@
+# Write a program to Calculate sum of first N natural numbers.
+
+N = int(input("Enter a positive integer N: "))
+
+print("sum of N natural no.:", N*(N+1)//2)
+# Just addint one more line
